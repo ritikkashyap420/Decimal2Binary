8a3fc3691e42aab4ebd5662b0755f87ab455f440
ritikkashyap420 <57295935+ritikkashyap420@users.noreply.github.com>
2020-10-09 02:14:36 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,9 +4,14 @@
 function myFunction() {
   var number=parseInt(document.getElementById("input").value);
   console.log(number);
-  var result = number.toString(2);
-  console.log(result);
-  document.getElementById("answer").value=result;
+  var resultA = number.toString(2);
+  var resultB = number.toString(8);
+  var resultC = number.toString(16);
+
+  document.getElementById("answerA").value=resultA;
+  document.getElementById("answerB").value=resultB;
+  document.getElementById("answerC").value=resultC;
+
 }
 
 document.getElementById("calculate").addEventListener("click", myFunction);
